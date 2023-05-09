87e1698f2ce9cbc0270ef45df583e89bd8b13712
Mrsteve <lan0193766@nuce.edu.vn>
2023-05-09 08:04:49 +0700

    first commit

---
+++ JavaApplication24/src/javaapplication24/MainClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication24;
+
+/**
+ *
+ * @author ngohoanglan2k3
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        LoginJFrame login = new LoginJFrame();
+        login.setVisible(true);
+    }
+}
